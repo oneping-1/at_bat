4f5e77f9c6a12d0c486b120695ab896642eef768
oneping-1 <coen.evans@gmail.com>
2026-03-21 21:18:24 -0400

    feat: implement FIFO queue class and integrate safe string output in live_stats

--- examples/live_stats.py
+++ examples/live_stats.py
@@ -17,15 +17,96 @@ Functions:
 
 import curses
 import argparse
-from typing import Tuple
+from typing import Any, Optional, Tuple
 from at_bat.game import Game, PlayEvents, AllPlays
 from at_bat.game_parser import GameParser
 from at_bat.statsapi_plus import get_re640_dataframe
 from at_bat.umpire import Umpire
 from at_bat.runners import Runners
-from at_bat.fifo import FIFO
 
 re640 = get_re640_dataframe()
+
+class FIFO:
+    """
+    Acts as a First In First Out (FIFO) queue
+    """
+    def __init__(self, max_length: int = 5):
+        self.max_length: int = max_length
+        self.list = []
+
+    def push(self, item) -> Optional[Any]:
+        """
+        Pushes a new item into the queue. If the length of the list is
+        a its max length, it will remove the oldest item in the queue
+
+        Args:
+            item (Any): The newest item to be added to the list
+
+        Returns:
+            Any: The oldest item in the list that was removed to make
+                room for the newest item. Returns None if no item was
+                removed.
+        """
+        removed_item = None
+        if len(self.list) == self.max_length:
+            removed_item = self.remove()
+        self.list.append(item)
+        return removed_item
+
+    def remove(self) -> Optional[Any]:
+        """
+        Removes the first (oldest) item in the list
+
+        Returns:
+            Any: The first item in the list
+        """
+        if len(self.list) > 0:
+            return self.list.pop(0)
+        return None
+
+    def contains(self, item) -> bool:
+        """
+        Compares the item argument to the each item in the list and
+        returns True if any of the items in the list equal the argument
+
+        Args:
+            item (Any): Item to be compared to the list items
+
+        Returns:
+            bool: Whether there is a match in the list
+        """
+        return item in self.list
+
+    def peak(self) -> Optional[Any]:
+        """
+        Peaks at the next item to be removed from the queue list
+
+        Returns:
+            Any: Item next to be removed. Returns None if is empty.
+        """
+        if len(self.list) > 0:
+            return self.list[0]
+        return None
+
+    def __len__(self):
+        return len(self.list)
+
+    def __repr__(self):
+        return f'{len(self.list)}'
+
+
+def safe_addstr(win, y: int, x: int, text: str):
+    """Write text safely with bounds checking and truncation."""
+    max_y, max_x = win.getmaxyx()
+    if y < 0 or y >= max_y or x < 0 or x >= max_x:
+        return
+
+    trimmed = text[:max_x - x]
+    try:
+        win.addstr(y, x, trimmed)
+    except curses.error:
+        pass
+
 
 def print_last_pitch(gamePk: int = None, delay_seconds: float = 0):
     """
@@ -58,7 +139,6 @@ def print_last_pitch(gamePk: int = None, delay_seconds: float = 0):
         raise ValueError('gamePk not provided')
 
     clr = ' ' * 40
-
     god = curses.initscr()
     god.clear()
     fifo = FIFO(5)
@@ -75,31 +155,31 @@ def print_last_pitch(gamePk: int = None, delay_seconds: float = 0):
 
                 i = 0
                 for line in _get_game_details(game, at_bat):
-                    god.addstr(i, 0, f'{line} {clr}')
+                    safe_addstr(god, i, 0, f'{line} {clr}')
                     i += 1
 
                 for line in _get_at_bat_details(at_bat, pitch):
-                    god.addstr(i, 0, f'{line} {clr}')
+                    safe_addstr(god, i, 0, f'{line} {clr}')
                     i += 1
 
                 i += 1
                 for line in _get_run_details(at_bat, pitch):
-                    god.addstr(i, 0, f'{line} {clr}')
+                    safe_addstr(god, i, 0, f'{line} {clr}')
                     i += 1
 
                 i += 1
                 for line in _get_umpire_details(game):
-                    god.addstr(i, 0, f'{line} {clr}')
+                    safe_addstr(god, i, 0, f'{line} {clr}')
                     i += 1
 
                 i += 1
                 for line in _get_pitch_details(pitch):
-                    god.addstr(i, 0, f'{line} {clr}')
+                    safe_addstr(god, i, 0, f'{line} {clr}')
                     i += 1
 
                 i += 1
                 for line in _get_hit_details(pitch):
-                    god.addstr(i, 0, f'{line} {clr}')
+                    safe_addstr(god, i, 0, f'{line} {clr}')
                     i += 1
 
                 god.refresh()
